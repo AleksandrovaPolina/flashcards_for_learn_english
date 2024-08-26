37f323e36fe88e2a73fb3f4f0f5d6b3ea65b9a07
AleksandrovaPolina <polina086@mail.ru>
2024-08-26 17:05:30 +0200

    fix code in Game component

--- src/Pages/Game/Game.jsx
+++ src/Pages/Game/Game.jsx
@@ -1,15 +1,13 @@
 import styles from "./Game.module.scss";
 import Word from "../../Components/Word/Word";
-import { useState } from "react";
-import { useContext } from "react";
-import { MyContext } from "../../Context/Context";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getWords } from "../../store/slice/words";
 
 export default function Game() {
   const [index, setIndex] = useState(0);
 
   const [count, setCount] = useState(0);
-
-  const { words } = useContext(MyContext);
 
   function increaseCount() {
     setCount(count + 1);
@@ -29,8 +27,21 @@ export default function Game() {
     setIndex(index - 1);
   }
 
+  const words = useSelector(state => state.words.words);
+  const {status, error} = useSelector(state => state.words)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(getWords())
+  }, [dispatch])
+
   return (
     <div className={styles.container_game}>
+      {status === 'loading' && (
+          <div className={styles.cssload_container}>
+            <div className={styles.cssload_whirlpool}></div>
+          </div>)}
+          {error && <h2>Ошибка: {error}</h2>}
       <div className={styles.wrapper}>
         <button className={styles.btn} onClick={scrollBack}>
           &larr;
